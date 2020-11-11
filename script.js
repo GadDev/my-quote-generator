c0d3ae1bdeb0ea9618b15a43cf0ed6271d82aa5a
algadaix <alexandre@qured.com>
2020-11-11 22:22:11 +0000

    adding const dom element

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 'use strict';
+const quoteContainer = document.getElementById('quote-gen-wrapper');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 // Get quote from APi
-
 async function getQuote() {
-	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	const proxyUrl = 'https://mysterious-retreat-29857.herokuapp.com/';
 	const apiURL =
 		'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
 	try {
 		const response = await fetch(proxyUrl + apiURL);
 		const data = await response.json();
-		console.log(data);
+		authorText.innerText = data.quoteAuthor;
+		quoteText.innerText = data.quoteText;
 	} catch (error) {
+		getQuotes();
 		console.log(error);
 		throw error;
 	}
