06b2e50eab5e1d9d8c674dbfaccb98858457e0e7
algadaix <alexandre@qured.com>
2020-11-11 21:57:27 +0000

    implement api forismatic and proxy

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict';
-console.log('Hello world');
+// Get quote from APi
+
+async function getQuote() {
+	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+	const apiURL =
+		'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
+	try {
+		const response = await fetch(proxyUrl + apiURL);
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
+
+// on load
+getQuote();
